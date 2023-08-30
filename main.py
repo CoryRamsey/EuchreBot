6d2e3794d8c77141c049f6ee658968281f2192c0
CoryRamsey <ramsey.cory@gmail.com>
2023-08-30 09:47:31 -0700

    Safe Discord Token

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import discord
+import os
 from discord.ext import commands
+from dotenv import load_dotenv
 
 intents = discord.Intents.default()
 intents.typing = False
 intents.presences = False
 
+load_dotenv()
+
 bot = commands.Bot(command_prefix='!', intents=intents)
+token = os.environ.get('DISCORD_TOKEN')
 
 @bot.event
 async def on_ready():
@@ -17,4 +22,5 @@ async def start_game(ctx):
     await ctx.send("Starting a new Euchre game!")
 
 
-bot.run('YOUR_TOKEN')
+bot.run(token)
+
