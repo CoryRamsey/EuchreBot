8f62106c9ce6a9ab67d9a48d690ec8e282f0bbd7
CoryRamsey <ramsey.cory@gmail.com>
2023-08-30 09:42:21 -0700

    init

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')
+
+@bot.command()
+async def start_game(ctx):
+
+    await ctx.send("Starting a new Euchre game!")
+
+
+bot.run('YOUR_TOKEN')
